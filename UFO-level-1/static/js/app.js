17b03a447222404a1e59ad4bc6b1d27c0b19faa8
K Ruhl <ruhlk@hotmail.com>
2020-10-09 21:36:28 -0500

    started js code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
-var tableData = data;
+let tableData = data;
 
 // YOUR CODE HERE!
+// Select the button
+let button = d3.select("#filter-btn");
+
+// Select the form
+let form = d3.select("#form-group");
+
+// Create even handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
