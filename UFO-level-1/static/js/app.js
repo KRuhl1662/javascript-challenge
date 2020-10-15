b2e4d595e3ef6d98d2d4b690ef0e587afc31654c
K Ruhl <ruhlk@hotmail.com>
2020-10-15 17:48:20 -0500

    making progress on the code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,5 +9,34 @@ let button = d3.select("#filter-btn");
 let form = d3.select("#form-group");
 
 // Create even handlers
-button.on("click", runEnter);
-form.on("submit", runEnter);
+button.on("click", runFilter);
+form.on("submit", runFilter);
+
+// Populate table with allllll the data
+// first select the table
+let ufoTable = d3.select("tbody");
+
+// now add data
+
+
+
+
+
+
+// function that takes input and filters data
+function runFilter(){
+
+    // Prevents the page from refreshing so it doesn't reset the html
+    d3.event.preventDefault();
+
+    // Select the date from the input
+    let inputDate = d3.select("#datetime");
+
+    // Get the value of datetime
+    let inputValue = inputDate.property('value')
+    console.log(inputValue);
+
+    // Use the form input to filter the data by date
+
+    let filteredUFOData = tableData.filter(sighting => sighting.datetime === inputValue);
+}
